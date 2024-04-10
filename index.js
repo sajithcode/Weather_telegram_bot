ffa259f6ba142f2983c3a9790378ac144aa037cb
sajith Rathna Sampath <sajithsampath721@gmail.com>
2024-04-10 16:42:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const TelegramBot = require('node-telegram-bot-api');
+const axios = require("axios");
+
+const token = '7198643418:AAHSyJgmWN-zH92n9E3yXv9VyIWPi8IyVRc';
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on("message", async (msg) => {
+    const chatId = msg.chat.id;
+    const userInput = msg.text;
+  
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=ea05f0b6617d998492f421c4335d3bba`
+      );
+      const data = response.data;
+      const weather = data.weather[0].description;
+      const temperature = data.main.temp - 273.15;
+      const city = data.name;
+      const humidity = data.main.humidity;
+      const pressure = data.main.pressure;
+      const windSpeed = data.wind.speed;
+      const message = `The weather in ${city} is ${weather} with a temperature of ${temperature.toFixed(2)}°C. The humidity is ${humidity}%, the pressure is ${pressure}hPa, and the wind speed is ${windSpeed}m/s.`;
+  
+      bot.sendMessage(chatId, message);
+    } catch (error) {
+      bot.sendMessage(chatId, "City doesn't exist.");
+    }
+  });
